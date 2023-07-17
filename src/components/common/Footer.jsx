60deedcedc1c2d6555af654af8d451bca86a0222
Agustin Baza <agustinbaza10@gmail.com>
2023-07-17 13:52:54 -0300

    Cree y linkee la pagina de estilos del footer, y la probe en el app.jsx

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,13 @@
+import "../../css/footer.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const Footer = () => {
+    return (
+     <>
+        <h1> HOLA </h1>
+     </>
+    );
+  };
+  
+  export default Footer;
+  
