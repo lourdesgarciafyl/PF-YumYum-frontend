380b80b4f409b47cf58864c40329494485245400
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-18 10:26:51 -0300

    se limpia App jsx luego de probar el componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Footer from "./components/common/Footer";;
 import Navegacion from "./components/common/Navegacion"
-import ItemProducto from "./components/views/producto/ItemProducto";
-import AdministrarProducto from "./components/views/AdministrarProductos";
+
 function App() {
   return (
     <>
         
           <Navegacion></Navegacion>
-          <AdministrarProducto></AdministrarProducto>
           <Footer></Footer>
     </>
   );
