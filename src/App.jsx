a1519607c17282d02ebeb8c150b4f854d82d32b2
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-07-18 16:43:23 -0300

    se sacó el componente login de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Footer from './components/common/Footer';
-import Navegacion from './components/common/Navegacion';
-import Login from './components/views/Login';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Footer from "./components/common/Footer";;
+import Navegacion from "./components/common/Navegacion"
 
 function App() {
   return (
     <>
-      <Navegacion></Navegacion>
-      <Login></Login>
-      <Footer></Footer>
+        
+          <Navegacion></Navegacion>
+          <Footer></Footer>
     </>
   );
 }
