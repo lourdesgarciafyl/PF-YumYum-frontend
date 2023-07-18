026b8558c41d9315f634ba22386b828f9bd9933d
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-07-18 19:23:44 -0300

    se hizo la querie de login de un usuario

--- src/components/helpers/queriesUsuario.js
+++ src/components/helpers/queriesUsuario.js
@@ -41,3 +41,26 @@ export const borrarUsuario = async (id) => {
       return null;
     }
   };
+
+  export const loginUsuario = async(usuario)=>{
+    try{
+        const respuesta = await fetch(URLUsuario);
+        const listaUsuarios = await respuesta.json();
+        //buscar cual usuario tiene el mail
+        const usuarioBuscado = listaUsuarios.find((itemUsuario)=> itemUsuario.email === usuario.email);
+        if(usuarioBuscado){
+            console.log('email encontrado');
+            if(usuarioBuscado.password === usuario.password){
+                return usuarioBuscado;
+            }else{
+                console.log('el password es incorrecto');
+                return null;
+            }
+        }else{
+            console.log('el email no existe');
+            return null
+        }       
+    }catch(error){
+        console.log(error)
+    }
+}
