6ec8b8f1e1be235c9b4fa8f36ff4820e698e2dae
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-07-18 20:02:14 -0300

    se agregó una validación de que el password tiene que ser entre 8 y 16 caracteres, un dígito, una minúscula y una mayúscula

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -63,9 +63,9 @@ const Login = () => {
                   {...register('password', {
                     required: 'La contraseña es un dato obligatorio.',
                     pattern: {
-                      value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8}$/,
+                      value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
                       message:
-                        'La contraseña debe tener mínimo 8 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.',
+                      'El password debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.',
                     },
                   })}
                 />
