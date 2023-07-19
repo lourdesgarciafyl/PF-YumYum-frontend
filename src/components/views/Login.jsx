233dfc31961f75e8dd8b297051d5a53caa0b8d6f
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-07-19 12:49:48 -0300

    se puso el mismo mensaje del campo Password cuando no valida igual al campo password del Formulario de Registro.

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -86,7 +86,7 @@ const Login = () => {
                     pattern: {
                       value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
                       message:
-                      'El password debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.',
+                      'La contraseña debe tener entre 8 y 16 caracteres, al menos un número, una minúscula, una mayúscula y no contener caracteres especiales.',
                     },
                   })}
                 />
