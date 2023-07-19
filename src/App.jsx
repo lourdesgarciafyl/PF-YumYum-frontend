495e5b913cca5119cd79557c0a379d9804d4523e
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-19 11:14:51 -0300

    se limpia app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Footer from "./components/common/Footer";;
 import Navegacion from "./components/common/Navegacion"
-import Registro from "./components/views/Registro";
 
 function App() {
   return (
     <>
         
           <Navegacion></Navegacion>
-         <Registro></Registro>
           <Footer></Footer>
     </>
   );
