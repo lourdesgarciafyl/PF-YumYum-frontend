2cbeafea56597bf1fb19453bd49cfbb112c77cab
Agustin Baza <agustinbaza10@gmail.com>
2023-07-19 18:09:33 -0300

    Se elimino la etiqueta hr y se agrego margin para mejorar la estetica

--- src/components/views/AdministrarUsuarios.jsx
+++ src/components/views/AdministrarUsuarios.jsx
@@ -15,11 +15,11 @@ const AdministradorUsuarios = () => {
   return (
     <div className="container mainSection my-4">
       <section>
-        <div className="d-flex justify-content-between align-items-center mt-5">
+        <div className="d-flex justify-content-between align-items-center mt-5 mb-2">
           <h1 className="display-4 letraAmarilla">Usuarios</h1>
           <button className="btn btn-warning">Agregar</button>
         </div>
-        <hr />
+
         <Table responsive striped bordered hover>
           <thead>
             <tr>
--- src/components/views/usuario/ItemUsuario.jsx
+++ src/components/views/usuario/ItemUsuario.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { Button } from "react-bootstrap";
 
 const ItemUsuario = () => {
-
   return (
     <tr>
       <td>Juan Perez</td>
       <td>juanperez@gmail.com</td>
       <td>Activo</td>
       <td>Cliente</td>
-      
+
       <td className="d-flex justify-content-around">
         <Button variant="warning" className="m-1">
           Editar
