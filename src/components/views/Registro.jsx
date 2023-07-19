e4287be04a88f468e1587e063e31764e75dcc4b9
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-18 23:05:41 -0300

    se agregan estilos css faltantes, en el form control y color de fondo blanco

--- src/components/views/Registro.jsx
+++ src/components/views/Registro.jsx
@@ -13,12 +13,14 @@ const Registro = () => {
             <Row className="py-2">
                 <Col lg={8}>
                     <Card className="letraForm">
-                        <Card.Title> Registro </Card.Title>
-                        <Card.Body>
-                            <Form>
+                        <Card.Title className="mt-4 mb-1 text-center letraSpace letraNaranja fw-bold fs-4">Registro</Card.Title>
+                        <hr className="letraNaranja" />
+                        <Card.Body className="d-flex justify-content-center">
+                            <Form className="w-75 letraRoboto letraRoja fs-5 fw-bold">
                             <Form.Group className="mb-3">
                                 <Form.Label>Nombre/s</Form.Label>
                                 <Form.Control
+                                className="inputFormRegistro"
                                 type="text"
                                 placeholder="Ej: Lisandro"></Form.Control>
                             </Form.Group>
@@ -26,6 +28,7 @@ const Registro = () => {
                             <Form.Group className="mb-3">
                                 <Form.Label>Apellido/s</Form.Label>
                                 <Form.Control
+                                className="inputFormRegistro"
                                 type="text"
                                 placeholder="Ej: Villafañe"></Form.Control>
                             </Form.Group>
@@ -33,6 +36,7 @@ const Registro = () => {
                             <Form.Group className="mb-3">
                                 <Form.Label>Email</Form.Label>
                                 <Form.Control
+                                className="inputFormRegistro"
                                 type="email"
                                 placeholder="Ej: lisandrov@gmail.com"></Form.Control>
                             </Form.Group>
@@ -40,6 +44,7 @@ const Registro = () => {
                             <Form.Group className="mb-3">
                                 <Form.Label>Contraseña</Form.Label>
                                 <Form.Control
+                                className="inputFormRegistro"
                                 type="password"
                                 placeholder="Ingresa una contraseña"></Form.Control>
                             </Form.Group>
@@ -47,11 +52,12 @@ const Registro = () => {
                             <Form.Group className="mb-3">
                                 <Form.Label>Confirmar contraseña</Form.Label>
                                 <Form.Control
+                                className="inputFormRegistro"
                                 type="password"
                                 placeholder="Ingresa nuevamente la contraseña"></Form.Control>
                             </Form.Group>
 
-                            <Button className="w-100">Registrarme</Button>
+                            <Button className="w-100 btnRegistrarme fw-bold">REGISTRARME</Button>
                             </Form>                            
                         </Card.Body>
                     </Card>
