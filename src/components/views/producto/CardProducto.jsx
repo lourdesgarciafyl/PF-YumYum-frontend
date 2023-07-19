855fc4f258bb0aea4ca43563fca9d1c994df2f71
Cristian Quiroga <cristianq3@gmail.com>
2023-07-19 05:58:31 -0300

    Se corrigen detalles de colores y alineacion

--- src/components/views/producto/CardProducto.jsx
+++ src/components/views/producto/CardProducto.jsx
@@ -13,28 +13,31 @@ const CardProducto = () => {
     setmostrarElementos(false);
   };
 
-
-
-
-
   return (
     <Col
       md={6}
       lg={3}
       className="mb-3"
       onMouseEnter={handleMouseEnter}
-  
       onMouseLeave={handleMouseLeave}
     >
-      <Card className="">
+      <Card className="letraRoboto">
         <Card.Title>Doble Cheese Burger con Bacon</Card.Title>
         <Card.Img
           variant="top"
-          src="https://res.cloudinary.com/dvcq6vatc/image/upload/v1689383720/yumyum/hamburguesaDobleCheddar_wyifm1.png" className="position-reltive"
+          src="https://res.cloudinary.com/dvcq6vatc/image/upload/v1689383720/yumyum/hamburguesaDobleCheddar_wyifm1.png"
+          className="position-relative"
         />
-        <Card.Body className={mostrarElementos ? "d-block d-lg-block" : "d-none d-lg-none"}>
+        <Card.Body
+          className={
+            mostrarElementos ? "d-block d-lg-block" : "d-none d-lg-none"
+          }
+        >
           <div className="justify-content-around flex-column align-items-center w-100 d-flex">
-            <div className="fw-bolder position-absolute precio text-dark"><p>$2999 </p> <p className="detalleLink">Ver Detalle </p></div>
+            <div className="fw-bolder position-absolute precio">
+              <p className="text-center fw-bold">$2999 </p>{" "}
+              <p className="detalleLink">Ver Detalle </p>
+            </div>
             <Plus className=" verMas shadow"></Plus>{" "}
           </div>
         </Card.Body>
