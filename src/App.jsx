26654c2d30fc9404c732f86735e9c4b84ec9cd54
Agustin Baza <agustinbaza10@gmail.com>
2023-07-19 21:04:01 -0300

    Se corriguio app.jsx como estaba antes, usado para probar pagina de error404

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Footer from "./components/common/Footer";
 import Navegacion from "./components/common/Navegacion";
-import Error404 from "./components/views/Error404";
+import Login from "./components/views/Login";
 
 function App() {
   return (
     <>
       <Navegacion></Navegacion>
-      <Error404></Error404>
+      <Login></Login>
       <Footer></Footer>
     </>
   );
