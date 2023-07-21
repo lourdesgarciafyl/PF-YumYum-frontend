5156027fef048eb4ea2d116a889c707133615935
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-07-21 12:08:47 -0300

    se importó el componente ItemPedido.jsx en AdministrarPedidos.jsx

--- src/components/views/AdministrarPedidos.jsx
+++ src/components/views/AdministrarPedidos.jsx
@@ -1,8 +1,23 @@
-
+import { Container, Row } from 'react-bootstrap';
+import ItemPedido from './pedido/ItemPedido';
 const AdministrarPedidos = () => {
   return (
-    <h2>AdministrarPedidos</h2>
-  )
-}
+    <section className="mainSection letraRoboto mb-3">
+      <Container>
+        <h1 className="display-4 mt-3 letraSpace text-white text-center text-md-start ">
+          Administrar Pedidos
+        </h1>
+        <hr />
+        <Row className="justify-content-start">
+          <ItemPedido></ItemPedido>
+          <ItemPedido></ItemPedido>
+          <ItemPedido></ItemPedido>
+          <ItemPedido></ItemPedido>
+          <ItemPedido></ItemPedido>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
-export default AdministrarPedidos
+export default AdministrarPedidos;
