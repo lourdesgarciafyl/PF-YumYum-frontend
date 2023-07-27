fbea301b61c6373e0e9029165f2605f47e9e5fa3
Cristian Quiroga <cristianq3@gmail.com>
2023-07-27 02:01:57 -0300

    Se agrega logica de borrar usuario

--- src/components/views/usuario/ItemUsuario.jsx
+++ src/components/views/usuario/ItemUsuario.jsx
@@ -1,14 +1,47 @@
-import React from "react";
 import { Button } from "react-bootstrap";
-  // import { Link } from 'react-router-dom';
-  import {editarUsuario, borrarUsuario, obtenerUsuario, consultaListaUsuarios} from "../../helpers/queriesUsuario"
+import Swal from "sweetalert2";
+// import { Link } from 'react-router-dom';
+import {
+  editarUsuario,
+  borrarUsuario,
+  consultaListaUsuarios,
+} from "../../helpers/queriesUsuario";
 
-const ItemUsuario = ({usuario, setUsuarios}) => {
-
-
-
-
-
+const ItemUsuario = ({ usuario, setUsuarios }) => {
+  const borrarUsuario1 = () => {
+    Swal.fire({
+      title: `¿Esta seguro de borrar el usuario ${usuario.nombreUsuario}?`,
+      text: "No se puede revertir este paso",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        //borrar el usuario
+        borrarUsuario(usuario.id).then((respuesta) => {
+          console.log(respuesta);
+          if (respuesta.status === 200) {
+            Swal.fire(
+              "Usuario eliminado",
+              `El ${usuario.nombreUsuario} fue eliminado correctamente`,
+              "success"
+            );
+            //actualizar la tabla de usuarios
+            consultaListaUsuarios().then((respuesta) => setUsuarios(respuesta));
+          } else {
+            Swal.fire(
+              "Ocurrio un error",
+              `Intente realizar esta operación nuevamente mas tarde`,
+              "success"
+            );
+          }
+        });
+      }
+    });
+  };
 
   return (
     <tr>
@@ -18,12 +51,10 @@ const ItemUsuario = ({usuario, setUsuarios}) => {
       <td>{usuario.perfil}</td>
 
       <td className="d-flex justify-content-around">
-        
-        <Button variant="warning" className="m-1">.
-   
-          Editar
+        <Button variant="warning" className="m-1">
+          . Editar
         </Button>
-        <Button variant="danger" className="m-1">
+        <Button variant="danger" className="m-1" onClick={borrarUsuario1}>
           Borrar
         </Button>
       </td>
