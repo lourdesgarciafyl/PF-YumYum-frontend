fc73289d2da52bdc2e9e48a1e286b57e60c7d735
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-07-28 19:36:48 -0300

    se cambió la función total por la suma de subtotalItem de cada producto del carrito. y se llamó la función total en el CarritoPedido

--- src/components/views/CarritoPedido.jsx
+++ src/components/views/CarritoPedido.jsx
@@ -2,7 +2,7 @@ import './../../css/carritoPedido.css';
 import { Container, Row, Col, Button, ListGroup } from 'react-bootstrap';
 import CardItemCarrito from './pedido/CardItemCarrito';
 import { crearPedido } from "../helpers/queriesPedido"
-import { subtotal, sumaCantidad } from '../helpers/queriesCarrito';
+import { total } from '../helpers/queriesCarrito';
 import { useEffect } from 'react';
 
 const CarritoPedido = ({usuario, carrito, setCarrito, totalProductos}) => {
@@ -47,7 +47,7 @@ const CarritoPedido = ({usuario, carrito, setCarrito, totalProductos}) => {
                 <ListGroup.Item className="border-0">
                   <div className="d-flex justify-content-between align-items-center">
                     <span className="letra-roboto tamanioLetra">Total:</span>
-                    <span className="letra-roboto tamanioLetra">$5800</span>
+                    <span className="letra-roboto tamanioLetra">${total(carrito)}</span>
                   </div>
                 </ListGroup.Item>
                 <ListGroup.Item className="border-0">
