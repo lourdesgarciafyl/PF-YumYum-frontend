4926675e6ad8bdabe7830de899cf8256c8262eb0
Cristian Quiroga <cristianq3@gmail.com>
2023-08-06 16:19:14 -0300

    Se completa nueva UI del Form de Login, se realiza Glasmorfismo.

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,6 +1,12 @@
 import { Form, Button, Container, Card, InputGroup } from "react-bootstrap";
 import { useForm } from "react-hook-form";
-import { PersonFill, FileLock2Fill } from "react-bootstrap-icons";
+import {
+  EnvelopeAt,
+   Google,
+  Facebook,
+  ShieldLock,
+    Person,
+} from "react-bootstrap-icons";
 import "./../../css/login.css";
 import { loginUsuario } from "../helpers/queriesUsuario";
 import Swal from "sweetalert2";
@@ -41,91 +47,115 @@ const Login = ({ setUsuarioLogueado, carrito }) => {
   };
 
   return (
-    <Container className="mainSection contenedorPrincipal">
-      <Card className="my-5 border-0 contenedor_login  bg-white p-3">
-        <Card.Title as="h5" className=" mt-3 text-center">
-          Iniciar Sesión
-        </Card.Title>
-        <Card.Body>
-          <Form noValidate onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group className="mb-3" controlId="formEmail">
-              <InputGroup>
-                <InputGroup.Text id="iconoEmail" className="fondoAmarillo">
-                  <PersonFill size={25}></PersonFill>
-                </InputGroup.Text>
-                <Form.Control
-                  aria-label="Email"
-                  aria-describedby="iconoEmail"
-                  type="email"
-                  className="inputFormulario"
-                  placeholder="Ej: lisandrov@gmail.com"
-                  maxLength={60} 
-                  {...register("email", {
-                    required: "El email es obligatorio",
-                    minLength: {
-                      value: 5,
-                      message: "Cantidad mínima de caracteres: 5",
-                    },
-                    maxLength: {
-                      value: 60,
-                      message: "Cantidad máxima de caracteres: 60.",
-                    },
-                    pattern: {
-                      value: /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/,
-                      message:
-                        "El Email debe cumplir con el formato juan@correo.com",
-                    },
-                  })}
-                />
-              </InputGroup>
-              <Form.Text className="text-danger my-2 py-3">
-                {errors.email?.message}
-              </Form.Text>
-            </Form.Group>
+    <>
+      <section className="mainSection contenedorPrincipal letraRoboto">
+        <Container className="  my-4 ">
+          <Card className="border-0 contenedor_login  bg-white p-2 my-3">
+            <Card.Title as="h5" className=" mt-3 text-center">
+              Iniciar Sesión
+            </Card.Title>
+            <Card.Body>
+              <Form noValidate onSubmit={handleSubmit(onSubmit)}>
+                <Form.Group className="mb-3" controlId="formEmail">
+                  <InputGroup>
+                    <InputGroup.Text
+                      id="iconoEmail"
+                      className="bg-transparent text-white"
+                    >
+                      <Person size={25}></Person>
+                    </InputGroup.Text>
+                    <Form.Control
+                      aria-label="Email"
+                      aria-describedby="iconoEmail"
+                      type="email"
+                      className="inputFormulario"
+                      placeholder="Ej: lisandrov@gmail.com"
+                      maxLength={60}
+                      {...register("email", {
+                        required: "El email es obligatorio",
+                        minLength: {
+                          value: 5,
+                          message: "Cantidad mínima de caracteres: 5",
+                        },
+                        maxLength: {
+                          value: 60,
+                          message: "Cantidad máxima de caracteres: 60.",
+                        },
+                        pattern: {
+                          value: /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/,
+                          message:
+                            "El Email debe cumplir con el formato juan@correo.com",
+                        },
+                      })}
+                    />
+                  </InputGroup>
+                  <Form.Text className="text-danger my-2 py-3">
+                    {errors.email?.message}
+                  </Form.Text>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formPassword">
-              <InputGroup>
-                <InputGroup.Text id="iconoPassword" className="fondoAmarillo">
-                  <FileLock2Fill size={25}></FileLock2Fill>
-                </InputGroup.Text>
-                <Form.Control
-                  placeholder="Contraseña"
-                  aria-label="Password"
-                  aria-describedby="iconoPassword"
-                  type="password"
-                  maxLength={16} 
-                  className="inputFormulario"
-                  {...register("password", {
-                    required: "La contraseña es un dato obligatorio.",
-                    pattern: {
-                      value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
-                      message:
-                        "La contraseña debe tener entre 8 y 16 caracteres, al menos un número, una minúscula, una mayúscula y no contener caracteres especiales.",
-                    },
-                  })}
-                />
-              </InputGroup>
-              <Form.Text className="text-danger my-2 py-3">
-                {errors.password?.message}
-              </Form.Text>
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formPassword">
+                  <InputGroup>
+                    <InputGroup.Text
+                      id="iconoPassword"
+                      className="bg-transparent text-white"
+                    >
+                      <ShieldLock size={25}></ShieldLock>
+                    </InputGroup.Text>
+                    <Form.Control
+                      placeholder="Contraseña"
+                      aria-label="Password"
+                      aria-describedby="iconoPassword"
+                      type="password"
+                      maxLength={16}
+                      className="inputFormulario"
+                      {...register("password", {
+                        required: "La contraseña es un dato obligatorio.",
+                        pattern: {
+                          value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                          message:
+                            "La contraseña debe tener entre 8 y 16 caracteres, al menos un número, una minúscula, una mayúscula y no contener caracteres especiales.",
+                        },
+                      })}
+                    />
+                  </InputGroup>
+                  <Form.Text className="text-danger my-2 py-3">
+                    {errors.password?.message}
+                  </Form.Text>
+                </Form.Group>
 
-            <Button type="submit" className="btn btn-dark w-100">
-              Ingresar
-            </Button>
-          </Form>
-          <hr />
-          <div className="text-center">
-            <Card.Text as="h6" className="mt-3 subtituloCuenta">
-              ¿No tienes una cuenta?
-            </Card.Text>
-            <Card.Link as={Link} to={"/registro"} className="btn btn-dark w-50">
-              Registrate
-            </Card.Link>
-          </div>
-        </Card.Body>
-      </Card>
-    </Container>
+                <Button type="submit" variant="outline-light" className=" w-100  btnIngreso">
+                  Ingresar
+                </Button>
+              </Form>
+        
+              <div className="text-center">
+                <Card.Text className="mt-2 m-0 p-0">¿No tienes una cuenta?</Card.Text>
+                <Card.Link
+                  as={Link}
+                  to={"/registro"}
+                  className="btn p-0 m-0 fw-bolder text-decoration-underline texto fs-6 letraAmarilla w-50"
+                >
+                  Registrate
+                </Card.Link>
+                <hr />
+              </div>
+              <Container className="mt-3">
+                <p className="text-center">Podés ingresar con: </p>
+                <ul className="d-flex justify-content-around fs-1  ">
+                  <Link to="/error404">
+                    <Google color="white"></Google>
+                  </Link>
+                  <Link to="/error404">
+                    <Facebook color="white"></Facebook>
+                  </Link>
+                </ul>
+              </Container>
+            </Card.Body>
+          </Card>
+        </Container>
+      </section>{" "}
+    </>
   );
 };
 
